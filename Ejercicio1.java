2147fa302a21b98481be3be3343fffbbeb70f00a
José Fernando <jfvillarb01@educarex.es>
2026-03-15 22:10:09 +0100

    Version 2

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -5,15 +5,22 @@ public class Ejercicio1 {
 
         Scanner sc = new Scanner(System.in);
         int[] numeros = new int[5];
+        int suma = 0;
 
         for (int i = 0; i < 5; i++) {
             System.out.print("Introduce un número: ");
             numeros[i] = sc.nextInt();
+            suma += numeros[i];
         }
 
         System.out.println("Números introducidos:");
         for (int n : numeros) {
             System.out.println(n);
         }
+
+        double media = (double) suma / numeros.length;
+
+        System.out.println("Suma: " + suma);
+        System.out.println("Media: " + media);
     }
 }
