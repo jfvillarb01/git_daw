d3e6e14a17ce6727920871973e122d76199891f8
José Fernando <jfvillarb01@educarex.es>
2026-03-15 22:45:05 +0100

    Version 4

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -1,50 +1,32 @@
 import java.util.Scanner;
 
 public class Ejercicio1 {
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        int[] numeros = new int[5];
 
-        int suma = 0;
-        int mayor;
-        int menor;
-        int positivos = 0;
-        int negativos = 0;
+        System.out.print("Cantidad de números: ");
+        int cantidad = sc.nextInt();
 
-        for (int i = 0; i < 5; i++) {
-            System.out.print("Introduce un número: ");
+        int[] numeros = new int[cantidad];
+
+        for (int i = 0; i < cantidad; i++) {
             numeros[i] = sc.nextInt();
-            suma += numeros[i];
         }
 
-        mayor = numeros[0];
-        menor = numeros[0];
+        for (int i = 0; i < numeros.length; i++) {
 
-        for (int n : numeros) {
+            int contador = 0;
 
-            if (n > mayor) {
-                mayor = n;
+            for (int j = 0; j < numeros.length; j++) {
+
+                if (numeros[i] == numeros[j]) {
+                    contador++;
+                }
             }
 
-            if (n < menor) {
-                menor = n;
-            }
-
-            if (n >= 0) {
-                positivos++;
-            } else {
-                negativos++;
-            }
+            System.out.println(numeros[i] + " se repite " + contador + " veces");
         }
-
-        double media = (double) suma / numeros.length;
-
-        System.out.println("Suma: " + suma);
-        System.out.println("Media: " + media);
-        System.out.println("Mayor: " + mayor);
-        System.out.println("Menor: " + menor);
-        System.out.println("Positivos: " + positivos);
-        System.out.println("Negativos: " + negativos);
     }
 }
