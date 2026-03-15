c11c9b81cbadcd85f354b47ee8ba0d8971a56d6d
José Fernando <jfvillarb01@educarex.es>
2026-03-15 23:16:45 +0100

    Version 4

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -1,26 +1,47 @@
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Ejercicio3 {
 
     public static void main(String[] args) {
 
-        Random rand = new Random();
+        Random r = new Random();
+
         int[] numeros = new int[10];
 
-        for (int i = 0; i < 10; i++) {
-            numeros[i] = rand.nextInt(100) + 1;
+        // Generar números aleatorios entre 1 y 100
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = r.nextInt(100) + 1;
         }
 
-        Arrays.sort(numeros);
+        ArrayList<Integer> pares = new ArrayList<>();
+        ArrayList<Integer> impares = new ArrayList<>();
 
-        System.out.println("Numeros pares:");
+        // Separar números pares e impares
+        for (int n : numeros) {
 
-        for (int i = numeros.length - 1; i >= 0; i--) {
+            if (n % 2 == 0)
+                pares.add(n);
+            else
+                impares.add(n);
 
-            if (numeros[i] % 2 == 0) {
-                System.out.println(numeros[i]);
-            }
         }
+
+        // Mostrar resultados
+        System.out.println("Números generados:");
+        for (int n : numeros) {
+            System.out.print(n + " ");
+        }
+
+        System.out.println("\n\nNúmeros pares:");
+        for (int p : pares) {
+            System.out.print(p + " ");
+        }
+
+        System.out.println("\n\nNúmeros impares:");
+        for (int i : impares) {
+            System.out.print(i + " ");
+        }
+
     }
 }
