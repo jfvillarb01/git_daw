f4e1aa10eca732d5e3ad422a924c1ec70efa19a9
José Fernando <jfvillarb01@educarex.es>
2026-03-15 23:02:33 +0100

    Version 4

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -1,29 +1,46 @@
 import java.util.Scanner;
 
 public class Ejercicio2 {
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+
         int[] edades = new int[5];
 
-        int mayores = 0;
-        int menores = 0;
+        int rango1 = 0; // 0-17
+        int rango2 = 0; // 18-30
+        int rango3 = 0; // 31-50
+        int rango4 = 0; // 51+
 
-        for (int i = 0; i < 5; i++) {
+        // Pedir edades
+        for (int i = 0; i < edades.length; i++) {
             System.out.print("Introduce una edad: ");
             edades[i] = sc.nextInt();
         }
 
+        // Clasificar edades
         for (int e : edades) {
 
-            if (e >= 18) {
-                mayores++;
+            if (e <= 17) {
+                rango1++;
+            } else if (e <= 30) {
+                rango2++;
+            } else if (e <= 50) {
+                rango3++;
             } else {
-                menores++;
+                rango4++;
             }
+
         }
 
-        System.out.println("Mayores de edad: " + mayores);
-        System.out.println("Menores de edad: " + menores);
+        // Mostrar resultados
+        System.out.println("\nResultados por rangos de edad:");
+        System.out.println("0 - 17 años: " + rango1);
+        System.out.println("18 - 30 años: " + rango2);
+        System.out.println("31 - 50 años: " + rango3);
+        System.out.println("51 o más años: " + rango4);
+
+        sc.close();
     }
 }
