24dbf18df3eb900da8efb77e0547f0edd4d18896
José Fernando <jfvillarb01@educarex.es>
2026-03-15 22:12:48 +0100

    Version 3

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -5,7 +5,12 @@ public class Ejercicio1 {
 
         Scanner sc = new Scanner(System.in);
         int[] numeros = new int[5];
+
         int suma = 0;
+        int mayor;
+        int menor;
+        int positivos = 0;
+        int negativos = 0;
 
         for (int i = 0; i < 5; i++) {
             System.out.print("Introduce un número: ");
@@ -13,14 +18,33 @@ public class Ejercicio1 {
             suma += numeros[i];
         }
 
-        System.out.println("Números introducidos:");
+        mayor = numeros[0];
+        menor = numeros[0];
+
         for (int n : numeros) {
-            System.out.println(n);
+
+            if (n > mayor) {
+                mayor = n;
+            }
+
+            if (n < menor) {
+                menor = n;
+            }
+
+            if (n >= 0) {
+                positivos++;
+            } else {
+                negativos++;
+            }
         }
 
         double media = (double) suma / numeros.length;
 
         System.out.println("Suma: " + suma);
         System.out.println("Media: " + media);
+        System.out.println("Mayor: " + mayor);
+        System.out.println("Menor: " + menor);
+        System.out.println("Positivos: " + positivos);
+        System.out.println("Negativos: " + negativos);
     }
 }
